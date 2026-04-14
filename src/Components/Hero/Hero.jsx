9a040a48abe48a2d85971009fea124109bb3d0dc
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-14 13:21:52 +0000

    add styling to avatar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 
 function Hero() {
 
-    let [avatarURL, setAvatarURL] = useState('');
+    let [avatarURL, setAvatarURL] = useState(null);
 
     // For getting the link of the profile image (avatar)
 
-    useEffect(()=>{
+    useEffect(() => {
         const getAvatarLink = async () => {
             let response = await fetch("https://api.github.com/users/lucky-KRMU");
             let data = await response.json();
@@ -23,8 +23,11 @@ function Hero() {
         <>
             <div className='w-full h-[85vh] bg-linear-to-b from-indigo-950 to-indigo-500 
             flex justify-center items-center'>
-                <div id="card" className='w-[80%] h-[90%] bg-indigo-800 rounded-4xl'>
-                    <img src={avatarURL} alt="Image of L. Pawar" className='' />
+                <div id="card" className='w-[80%] h-[90%] bg-indigo-800 rounded-4xl flex flex-col md:flex-row items-center justify-center gap-5'>
+                    <img src={avatarURL} alt="Image of L. Pawar" className='w-[80%] md:w-[30%] rounded-3xl border-10 border-indigo-200 border-double' />
+                    <div id="info">
+                        <h1>Lucky Pawar</h1>
+                    </div>
                 </div>
             </div>
         </>
