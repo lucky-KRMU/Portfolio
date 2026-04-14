1faec74ad37516c64ca4eec8c4eec221cb5b2c10
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-14 13:25:56 +0000

    add heading in hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -23,10 +23,10 @@ function Hero() {
         <>
             <div className='w-full h-[85vh] bg-linear-to-b from-indigo-950 to-indigo-500 
             flex justify-center items-center'>
-                <div id="card" className='w-[80%] h-[90%] bg-indigo-800 rounded-4xl flex flex-col md:flex-row items-center justify-center gap-5'>
+                <div id="card" className='w-[80%] h-[90%] bg-indigo-800 rounded-4xl flex flex-col md:flex-row items-center justify-center gap-10'>
                     <img src={avatarURL} alt="Image of L. Pawar" className='w-[80%] md:w-[30%] rounded-3xl border-10 border-indigo-200 border-double' />
                     <div id="info">
-                        <h1>Lucky Pawar</h1>
+                        <h1 className='text-3xl md:text-7xl font-[Onest] font-semibold text-white drop-shadow-[0_0_10px_rgba(255,255,255,0.9)] cursor-pointer'>Lucky Pawar</h1>
                     </div>
                 </div>
             </div>
