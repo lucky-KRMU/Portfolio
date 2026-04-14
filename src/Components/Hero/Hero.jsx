03bb3e03c1452cf04df3d49c71e84a1ca3b0b887
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-14 13:08:05 +0000

    create Hero Component and add basic style

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <>
+            <div className='w-full h-[85vh] bg-linear-to-b from-indigo-950 to-indigo-500 
+            flex justify-center items-center'>
+                <div id="card" className='w-[80%] h-[90%] bg-indigo-800 rounded-4xl'>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero
