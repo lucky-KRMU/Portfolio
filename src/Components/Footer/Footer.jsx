bbbe7fc4b44aae6d1c8661c89af37b94c6f78707
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-16 15:09:29 +0000

    add other links to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router';
-import { FaHeart, FaLinkedin  } from "react-icons/fa";
-import { FaSquareXTwitter, FaSquareGithub, FaSquareInstagram   } from "react-icons/fa6";
+import { FaHeart, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter, FaSquareGithub, FaSquareInstagram } from "react-icons/fa6";
 
 function Footer() {
     return (
         <>
             <footer className='w-full p-10 bg-indigo-950 flex flex-col items-center justify-center gap-5'>
-                <div id="footerLinks" className='grid grid-cols-1
+                <div id="footerLinks" className='grid grid-cols-1 gap-5
                 
-                md:grid-cols-2'>
+                md:grid-cols-2
+                md:gap-10
+                lg:gap-[40vw]'>
                     <div id="socialLinks" className='text-white text-xl font-[Fira_Sans] flex flex-col items-center justify-center gap-2
                     
                     md:gap-5'>
@@ -23,20 +25,39 @@ function Footer() {
                                 <FaSquareXTwitter className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
                             </Link>
                             <Link to="https://www.linkedin.com/in/luckypawar">
-                                <FaLinkedin className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]'  />
+                                <FaLinkedin className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
                             </Link>
                             <Link to="https://www.github.com/lucky-KRMU">
-                                <FaSquareGithub  className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
+                                <FaSquareGithub className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
                             </Link>
                             <Link to="https://www.instagram.com/luckythecoder">
-                                <FaSquareInstagram   className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
+                                <FaSquareInstagram className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
                             </Link>
                         </div>
                     </div>
-                    <div id="WebSiteLinks"></div>
+                    <div id="WebSiteLinks">
+                        <div  className='text-white font-[Fira_Sans] flex flex-col items-center justify-center gap-2
+                    
+                    md:gap-0.5'>
+                            <h3 className='text-2xl md:text-3xl'>Other Links</h3>
+                            <div className='grid grid-cols-2 gap-2 text-center text-l
+                            
+                            md:gap-0.5'>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Home</Link>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Projects</Link>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Contact</Link>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>About</Link>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Bio.</Link>
+                                <Link to="/home" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Skills</Link>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Download CV</Link>
+                                <Link to="" className='duration-200 underline-offset-4 hover:underline hover:text-sky-500'>Other</Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <h3 className='text-center font-[Onest] text-white'>&copy; 2026 Lucky Pawar.
-                    <div className='flex items-center justify-center gap-0.5'>Built with React, Tailwind, Curiosity and Love.<FaHeart className='text-red-600' /></div></h3>
+                    <div className='flex flex-col items-center justify-center gap-0.5
+                    md:flex-row'>Built with React, Tailwind, Curiosity and Love.<FaHeart className='text-red-600' /></div></h3>
             </footer>
         </>
     )
