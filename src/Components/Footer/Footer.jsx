c0a8943d811c4f2c765f9d989faa28f423ec9188
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-16 14:26:41 +0000

    add &copy; line in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+// import { Link } from 'react-router';
+import { FaHeart } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 function Footer() {
-  return (
-    <>
-    <footer>
-        This is footer
-    </footer>
-    </>
-  )
+    return (
+        <>
+            <footer className='w-full h-[30vh] p-10 bg-indigo-950 flex flex-col items-center justify-center gap-5'>
+                <div id="footerLinks" className='grid grid-cols-2'>
+                    <div id="socialLinks">
+                        {/* <Link to="https://www.x.com"> */}
+                            <FaSquareXTwitter />
+                        {/* </Link> */}
+                    </div>
+                    <div id="WebSiteLinks"></div>
+                </div>
+                <h3 className='text-center font-[Onest] text-white'>&copy; 2026 Lucky Pawar.
+                    <div className='flex items-center justify-center gap-0.5'>Built with React, Tailwind, Curiosity and Love.<FaHeart className='text-red-600' /></div></h3>
+            </footer>
+        </>
+    )
 }
 
 export default Footer
