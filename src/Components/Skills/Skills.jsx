f1a1e2bbb24227aef0cc8572a091d8280d20ff56
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-16 08:20:11 +0000

    add skill card in skills.jsx

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { FaReact } from "react-icons/fa";
+
+const SkillCard = ({icon, name}) => {
+    return(
+        <>
+        <div>
+        {icon}
+        <h6>{name}</h6>
+        </div>
+        </>
+    );
+}
 
 function Skills() {
   return (
     <>
     <section id="skills">
-        <div className='w-full bg-linear-to-b from-indigo-700 to-indigo-800 flex items-center justify-center'>
-            <h1 className=''>Skills</h1>
+        <div className='w-full p-10 bg-linear-to-b from-indigo-700 to-indigo-800 flex flex-col items-center justify-center gap-10'>
+            <h1 className='text-3xl font-semibold font-[Fira_Sans] text-white'>Skills</h1>
+            <div id="frontend">
+                <h4>Frontend</h4>
+                <SkillCard icon={<FaReact />} name="ReactJS" />
+            </div>
         </div>
     </section>
     </>
