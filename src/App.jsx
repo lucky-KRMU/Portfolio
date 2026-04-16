83bbe533fea30be4ae4064155a86fc16e444d069
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-16 14:31:12 +0000

    add react-router in app.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
 import Footer from './Components/Footer/Footer'
+import { Outlet } from 'react-router'
 
 function App() {
 
   return (
    <>
     <Header />
-    <Home />
+    <Outlet />
     <Footer />
    </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './Components/Home/Home.jsx'
+
+
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'home',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
