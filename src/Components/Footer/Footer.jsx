c30d3023e0a3f7452f0b06ab76761c97371b4c95
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-16 14:47:55 +0000

    add social links in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
-// import { Link } from 'react-router';
-import { FaHeart } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { Link } from 'react-router';
+import { FaHeart, FaLinkedin  } from "react-icons/fa";
+import { FaSquareXTwitter, FaSquareGithub, FaSquareInstagram   } from "react-icons/fa6";
 
 function Footer() {
     return (
         <>
-            <footer className='w-full h-[30vh] p-10 bg-indigo-950 flex flex-col items-center justify-center gap-5'>
-                <div id="footerLinks" className='grid grid-cols-2'>
-                    <div id="socialLinks">
-                        {/* <Link to="https://www.x.com"> */}
-                            <FaSquareXTwitter />
-                        {/* </Link> */}
+            <footer className='w-full p-10 bg-indigo-950 flex flex-col items-center justify-center gap-5'>
+                <div id="footerLinks" className='grid grid-cols-1
+                
+                md:grid-cols-2'>
+                    <div id="socialLinks" className='text-white text-xl font-[Fira_Sans] flex flex-col items-center justify-center gap-2
+                    
+                    md:gap-5'>
+                        <h3 className='text-2xl md:text-3xl'>Social Links</h3>
+                        <div className='flex flex-col gap-2 items-center justify-center cursor-pointer
+                        
+                        md:flex-row 
+                        md:text-3xl
+                        md:gap-10'>
+                            <Link to="https://www.x.com">
+                                <FaSquareXTwitter className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
+                            </Link>
+                            <Link to="https://www.linkedin.com/in/luckypawar">
+                                <FaLinkedin className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]'  />
+                            </Link>
+                            <Link to="https://www.github.com/lucky-KRMU">
+                                <FaSquareGithub  className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
+                            </Link>
+                            <Link to="https://www.instagram.com/luckythecoder">
+                                <FaSquareInstagram   className='duration-200 ease-in-out hover:text-sky-500 hover:scale-[1.2]' />
+                            </Link>
+                        </div>
                     </div>
                     <div id="WebSiteLinks"></div>
                 </div>
