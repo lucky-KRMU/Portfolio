e65e046e9a3bd2e2a11e0a81c7cb329ca92d7c3d
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-18 08:24:21 +0000

    style not found page

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+    <section id="notfound" className='w-full h-[50vh] bg-linear-to-b from-indigo-950 to-indigo-800 flex flex-col items-center justify-center text-white'>
+        <h1 className='text-8xl font-[Fira_Code] font-extrabold cursor-not-allowed
+        
+        lg:text-9xl'>404</h1>
+        <h3 className='text-4xl font-[Fira_Code] font-semibold animate-pulse duration-200 cursor-grab
+
+        hover:text-sky-500'>Not Found</h3>
+    </section>
+    </>
   )
 }
 
