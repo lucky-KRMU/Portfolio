89248dacd7c948604b2f4232b319b28b43f14ba6
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-18 08:15:37 +0000

    change router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { RouterProvider, createHashRouter } from 'react-router'
 import Home from './Components/Home/Home.jsx'
 import Projects from './Components/Projects/Projects.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 
 
-let router = createBrowserRouter([
+let router = createHashRouter([
   {
     path: '/',
     element: <App />,
