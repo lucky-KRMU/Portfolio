de893c61dd6c31b4b89bc515b57a15a2cfc270e1
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-18 06:57:39 +0000

    add h1 in contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { RiContactsFill } from "react-icons/ri";
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+    <section id="contact" className='w-full bg-linear-to-b from-indigo-950 to-indigo-800'>
+        <h1 className='flex items-center justify-center gap-1 font-[Onest] text-4xl font-semibold text-white cursor-pointer
+        py-10 duration-200
+
+        md:text-5xl
+        md:hover:gap-4'> <RiContactsFill />| <span>Contact</span></h1>
+    </section>
+    </>
   )
 }
 
