dfb0b1e4fc961c39a53b7a1ca910b2dfbfa55d14
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-18 03:28:26 +0000

    add heading in projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { MdConstruction } from "react-icons/md";
+
 
 function Projects() {
   return (
     <>
-    This is projects
+    <section id="projects" className='w-full p-5 bg-linear-to-b from-indigo-950 to-indigo-800'>
+      <h1 className='flex items-center justify-center gap-1 text-3xl font-[Onest] font-semibold text-white cursor-pointer duration-200
+      
+      md:text-4xl
+      md:hover:gap-4
+      md:hover:font-bold'><MdConstruction /> | <span>Projects</span></h1>
+
+    </section>
     </>
   )
 }
